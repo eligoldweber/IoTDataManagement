7634b6c15c43cecd78976e85ebcce3c414344f20
eligoldweber <eligoldweber@gmail.com>
2019-11-17 14:08:48 -0500

    Added RocksDB

--- faustTest.py
+++ faustTest.py
@@ -5,23 +5,25 @@ import pandas as pd
 import time
 import json
 import Record
+import rocksdb
+
 
 class Point(faust.Record, serializer='json'):
     ts: str
     temp: float
     id: int
     
-
 app = faust.App(
     'node-data',
     broker='kafka://localhost:9092',
     key_serializer='json',
+    store='rocksdb://',
 )
 
 rawDataTopic = app.topic('nodeInput',value_type=Point,value_serializer='json')
 CleanDataTopic = app.topic('clean-data',value_type=Point,value_serializer='json')
 CompressDataTopic = app.topic('compress-data',value_type=Point,value_serializer='json')
-
+db = rocksdb.DB("test.db", rocksdb.Options(create_if_missing=True))
 
 @app.agent(rawDataTopic)
 async def processData(rawData):
@@ -40,7 +42,8 @@ async def processCleanData(rawData):
 @app.agent(CompressDataTopic)
 async def processCompressData(cleanData):
     async for data in cleanData:
-        print(data.dumps())
+        db.put(bytes(str(data.id), encoding= 'utf-8'), bytes(str(data.dumps()), encoding= 'utf-8'))
+        print(db.get(bytes(str(data.id), encoding= 'utf-8')))
 
 @app.task()
 async def produce():
@@ -56,6 +59,27 @@ async def produce():
         time.sleep(.0500)
             
 
+@app.service
+class BackgroundService(Service):
+
+    async def on_start(self):
+        print('BACKGROUND SERVICE IS STARTING')
+        print('CURRENT Keys in DB:')
+        it = db.iterkeys()
+        it.seek_to_first()
+        # prints [b'key1', b'key2', b'key3']
+        print (list(it))
+
+    async def on_stop(self):
+        print('BACKGROUNDSERVICE IS STOPPING')
+        it = db.iterkeys()
+        it.seek_to_first()
+        for k in list(it):
+            print("Deleteing: {0}".format(k))
+            db.delete(k)
+
+
+            
             
 if __name__ == '__main__':
     app.main()
