5fc130bc35c61cc2d1786e0356e5fd97d995c526
eligoldweber <eligoldweber@gmail.com>
2019-12-06 12:03:04 -0500

    modularize DS and Bypass flags

---
+++ DSample.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+
+class DSample:
+	def __init__(self,sample,startK,max,window,p):
+		self.sampleRate = sample
+		self.k = startK
+		self.tMax = max
+		self.windowSize = window
+		self.phi = p
+		self.rawCount = 0
+		self.uid = 0
+		self.dfRaw = pd.DataFrame(columns=['id','val'])
+
+	def add_val(self, id,val):
+		if(self.rawCount % self.sampleRate == 0):
+			self.dfRaw.loc[self.rawCount] = [self.rawCount,val]
+			if(len(self.dfRaw.index) >= self.windowSize):
+				# More robust df that calcuates meta data about rolling frame
+				dfStats = self.dfRaw.copy()
+				dfStats['Rolling_Average'] = dfStats.iloc[:,1].rolling(window=self.windowSize).mean()
+				dfStats['Rolling_STD'] = dfStats.iloc[:,1].rolling(window=self.windowSize).std()
+				dfStats['bb_UP'] = dfStats.iloc[:,2] + (self.k*dfStats.iloc[:,3])
+				dfStats['bb_DOWN'] = dfStats.iloc[:,2] - (self.k*dfStats.iloc[:,3])
+
+				std = float(dfStats.tail(1)['Rolling_STD'])
+				up = float(dfStats.tail(1)['bb_UP'])
+				down = float(dfStats.tail(1)['bb_DOWN'])
+				dyn = round(2*self.k*std,4)
+				
+				assert dyn == round(abs(up - down),4)
+				rawsampleRate = (self.tMax)/(1+pow(dyn,self.phi))
+				# Cant sample less than every data point, so rouind up
+				if(rawsampleRate < 1):
+					self.sampleRate = math.ceil(rawsampleRate)
+				else:
+					self.sampleRate = round(rawsampleRate)
+				dfStats['sampleRate'] = self.sampleRate
+				self.dfRaw = self.dfRaw.drop(self.dfRaw.index[0])
+				tuplePoint =(self.uid,val)
+				# data.uid = uidCnt
+				self.uid = self.uid + 1
+				self.rawCount = self.rawCount + 1;
+				return tuplePoint
+			else:
+				tuplePoint =(self.uid,val)
+				self.uid = self.uid +1
+				self.rawCount = self.rawCount + 1;
+				return tuplePoint
+				
+		self.rawCount = self.rawCount + 1;
+		return False
+
+
+def main():
+	sample = DSample(1,.9,20,5,2)
+	print(sample.tMax)
+	print(sample.add_val(1,5))
+	print(sample.add_val(1,5))
+	print(sample.add_val(1,5))
+	print(sample.add_val(1,5))
+	print(sample.add_val(1,5888))
+	print(sample.add_val(1,5))
+	print(sample.add_val(1,5))
+	print(sample.add_val(1,5))
+	print(sample.add_val(1,5888))
+
+            
+            
+if __name__ == '__main__':
+    main()
--- faustTest.py
+++ faustTest.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 import zlib
 from MAD import MAD
 from KNN import KNN
-
+from DSample import DSample 
 
 #Constants -- try changing them to see differences 
 windowSize = 35
@@ -53,7 +53,9 @@ app = faust.App(
     key_serializer='json',
     store='rocksdb://',
 )
-
+SAMPLE_PASS = False
+CLEAN_PASS = True
+COMPRESS_PASS = True
 THRESHOLD = 5 #length difference in B+D to start new B
 LIMIT = 20 # Upper bound for B+D
 currentBase = CompressedPoint() 
@@ -64,62 +66,27 @@ db = rocksdb.DB("test.db", rocksdb.Options(create_if_missing=True,num_levels=1,t
 knn = KNN()
 mad = MAD()
 
+
 @app.agent(rawDataTopic)
 async def processData(rawData):
-    global sampleRate
-    global dfDynamicSample
-    dfRaw = pd.DataFrame(columns=['id','tempVal'])
-    i = 0
-    uidCnt = 0
-    chunksize = 1
-    async for data in rawData:
-		# Only 'save' data based on sampleRate (default is 1 ie every value is saved)
-        # print("-----> " + str(sampleRate) + " :: processing " + str(data.uid))
-        if(i % sampleRate == 0):
-			# Currently only saving an 'id' and 1 value 'Air Temp' (Needs to be adjusted for bigger examples)
-            dfRaw.loc[i] = [i,data.temp]
-            if(len(dfRaw.index) >= windowSize):
-                # More robust df that calcuates meta data about rolling frame
-                dfStats = dfRaw.copy()
-                dfStats['Rolling_Average'] = dfStats.iloc[:,1].rolling(window=windowSize).mean()
-                dfStats['Rolling_STD'] = dfStats.iloc[:,1].rolling(window=windowSize).std()
-                dfStats['bb_UP'] = dfStats.iloc[:,2] + (k*dfStats.iloc[:,3])
-                dfStats['bb_DOWN'] = dfStats.iloc[:,2] - (k*dfStats.iloc[:,3])
-                #Calc distance between upper BB and lower BB to use in sampling rate
-                std = float(dfStats.tail(1)['Rolling_STD'])
-                up = float(dfStats.tail(1)['bb_UP'])
-                down = float(dfStats.tail(1)['bb_DOWN'])
-                dyn = round(2*k*std,4)
-                assert dyn == round(abs(up - down),4)
-                rawsampleRate = (tMax)/(1+pow(dyn,phi))
-                # Cant sample less than every data point, so rouind up
-                if(rawsampleRate < 1):
-                    sampleRate = math.ceil(rawsampleRate)
-                else:
-                    sampleRate = round(rawsampleRate)
-                    
-                dfStats['sampleRate'] = sampleRate
-                # Add new row to DF to keep track of total data (Used for diagram -- not needed in real life)
-                dfDynamicSample =dfDynamicSample.append(dfStats.tail(1))
-                dfRaw = dfRaw.drop(dfRaw.index[0])
-                data.uid = uidCnt
-                uidCnt = uidCnt +1 
-                await CleanDataTopic.send(value=data)
-                time.sleep(.050)
-            else:
-                data.uid = uidCnt
-                uidCnt = uidCnt +1
-                await CleanDataTopic.send(value=data)
-                time.sleep(.050)
-        i = i + 1
-    # axDynamic = plt.gca()
-#     dfDynamicSample.plot(kind='line',x='id',y='tempVal',color='green',ax=axDynamic,figsize=(25,10))
-#     dfDynamicSample.plot(kind='line',x='id',y='Rolling_Average',color='black',ax=axDynamic,figsize=(25,10))
-#     dfDynamicSample.plot(kind='line',x='id',y='bb_UP',color='blue',ax=axDynamic)
-#     dfDynamicSample.plot(kind='line',x='id',y='bb_DOWN',color='red',ax=axDynamic)
-#     dfDynamicSample.plot(kind='line',x='id',y='sampleRate',color='orange',ax=axDynamic,alpha=0.5)
-#
-#     plt.savefig('isItWorking.png')
+	global sampleRate
+	global dfDynamicSample
+	dfRaw = pd.DataFrame(columns=['id','tempVal'])
+	i = 0
+	uidCnt = 0
+	chunksize = 1
+	dsample1 = DSample(sampleRate,k,tMax,windowSize,phi)
+	async for data in rawData:
+		if(SAMPLE_PASS):
+			await CleanDataTopic.send(value=data)
+		else:
+			sampled = dsample1.add_val(data.uid,data.temp)
+			if(sampled):
+				data.uid = sampled[0]
+				data.temp = sampled[1]
+				await CleanDataTopic.send(value=data)
+				time.sleep(.050)
+
 
 
 @app.agent(CleanDataTopic)
@@ -127,31 +94,24 @@ async def processCleanData(rawData):
 	global knn
 	global mad
 	async for data in rawData:
-        # print("Send to Compress")
-        # data is a point
-        
-        # # with KNN
- #        val = [data.temp]
- #        knn.add_number(val)
- #        if len(knn.data) < knn.k:
- #            await CompressDataTopic.send(value=data)
- #        else:
- #            if(not knn.outlier(val)):
- #                await CompressDataTopic.send(value=data)
-		#with MAD detection
-		mad.add_number(data.temp)
-		if not mad.outlier(data.temp):
+		if(CLEAN_PASS):
 			await CompressDataTopic.send(value=data)
-
-
-        
-# @app.agent(CompressDataTopic)
-# async def processCompressData(cleanData):
-# 	async for data in cleanData:
-# 		db.put(bytes(str(data.uid), encoding= 'utf-8'), bytes(str(data.dumps()), encoding= 'utf-8'))
-# 		print(db.get(bytes(str(data.uid), encoding= 'utf-8')))
-# 		stats = "[MONITOR] average runtime events: "+ str(app.monitor.events_runtime_avg)
-# 		print(stats)
+		else:
+		    # print("Send to Compress")
+		    # data is a point
+    
+		    # # with KNN
+		#        val = [data.temp]
+		#        knn.add_number(val)
+		#        if len(knn.data) < knn.k:
+		#            await CompressDataTopic.send(value=data)
+		#        else:
+		#            if(not knn.outlier(val)):
+		#                await CompressDataTopic.send(value=data)
+			#with MAD detection
+			mad.add_number(data.temp)
+			if not mad.outlier(data.temp):
+				await CompressDataTopic.send(value=data)
 
 
 @app.agent(CompressDataTopic)
@@ -162,27 +122,33 @@ async def processCompressDataNew(cleanData):
 	delta = []
 
 	async for data in cleanData:
-		if id == 1:
-			CompressedData.ts = currentBase.ts = convertDate(data.ts)
-			CompressedData.temp = currentBase.temp = data.temp
-			CompressedData.id = id
-			id = id + 1
-			delta = []
-			print(len(str(convertDate(data.ts) - currentBase.ts)))
-		elif len(str(convertDate(data.ts) - currentBase.ts)) > THRESHOLD or current == LIMIT:
-			putInDB(CompressedData,delta)
-			CompressedData.ts = currentBase.ts = convertDate(data.ts)
-			CompressedData.temp = currentBase.temp = data.temp
-			CompressedData.id = id
-			id = id + 1
-			delta = []
-			current = 1
+		if(COMPRESS_PASS):
+			db.put(bytes(str(data.uid), encoding= 'utf-8'), bytes(str(data.dumps()), encoding= 'utf-8'))
+			print(db.get(bytes(str(data.uid), encoding= 'utf-8')))
+			stats = "[MONITOR] average runtime events: "+ str(app.monitor.events_runtime_avg)
+			print(stats)
 		else:
-			tmpts = convertDate(data.ts) - currentBase.ts
-			delta.append(Delta(tmpts,round(data.temp - currentBase.temp, 3)))
-			current = current + 1
+			if id == 1:
+				CompressedData.ts = currentBase.ts = convertDate(data.ts)
+				CompressedData.temp = currentBase.temp = data.temp
+				CompressedData.id = id
+				id = id + 1
+				delta = []
+				print(len(str(convertDate(data.ts) - currentBase.ts)))
+			elif len(str(convertDate(data.ts) - currentBase.ts)) > THRESHOLD or current == LIMIT:
+				putInDB(CompressedData,delta)
+				CompressedData.ts = currentBase.ts = convertDate(data.ts)
+				CompressedData.temp = currentBase.temp = data.temp
+				CompressedData.id = id
+				id = id + 1
+				delta = []
+				current = 1
+			else:
+				tmpts = convertDate(data.ts) - currentBase.ts
+				delta.append(Delta(tmpts,round(data.temp - currentBase.temp, 3)))
+				current = current + 1
 
-	putInDB(CompressedData,delta)
+			putInDB(CompressedData,delta)
 			
 @app.task()
 async def produce():
